bc2215b9100a4564fd90f7a92eae1f637cd56a15
John Desmond <johndesmond631@gmail.com>
2025-04-15 21:46:19 -0400

    font size

--- ny-website/src/pages/HomePage.tsx
+++ ny-website/src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 function HomePage() {
     return (
     <div className="flex flex-col relative ml-4">
-        <h1 className="mb-4">Welcome!</h1>
-        <div className="mb-4 text-[10px]">
+        <h1 className="mb-4 text-lg">Welcome!</h1>
+        <div className="mb-4">
         This is an educational website containing information about climate change
             as it pertains to NY.  
         </div>
-        <div className="mb-4 text-[10px]">
+        <div className="mb-4">
             Click the burger to learn more.
         </div>
     </div>)
